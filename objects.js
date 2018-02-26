d55f16d4927795dd92883b815f7e9f887a4093db
hector ariceaga <hector_ariceaga@hotmail.com>
2018-02-26 21:49:32 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist {aristsNames: songTitles}
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist.artistNames[artistName];
+  playlist.songTitles[songTitle];
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistNames[artistName];
+  return playlist;
+}
